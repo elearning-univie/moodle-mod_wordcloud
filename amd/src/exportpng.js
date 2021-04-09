80da491af71effd810006d41f071422d82e0c61f
Adrian Czermak <adrian.czermak@univie.ac.at>
2021-04-09 09:47:40 +0200

    devcomit

--- amd/src/exportpng.js
+++ amd/src/exportpng.js
@@ -1,11 +1,11 @@
 import html2canvas from "lib/html2canvas/html2canvas.js";
-//import saveAs from "mod_wordcloud/filesaver";
+import saveAs from "mod_wordcloud/filesaver";
 import $ from "jquery";
 
 export const init = () => {
     $.mod_wordcloud_pic = function () {
         var wc_content = document.getElementById("mod-wordcloud-content");
-        var url;
+        //var url;
         html2canvas(wc_content, {
             scrollX: 0,
             scrollY: -window.scrollY + 10,
@@ -13,12 +13,12 @@ export const init = () => {
         }).then(canvas => {
             //var imgData = canvas.toDataURL('image/png');
             canvas.toBlob(function(blob) {
-                url = window.URL.createObjectURL(blob);
+                /*url = window.URL.createObjectURL(blob);
                 downloads.download({
                     'url': url,
                     'filename': 'file.png',
-                });
-                //saveAs(blob, "pretty_image.png");
+                });*/
+                saveAs(blob, "pretty_image.png");
             });
             //window.URL.revokeObjectURL(url);
             //window.open(imgData , "_blank");
