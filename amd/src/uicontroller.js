1f13c27fec4fa95c798d330940b639b43af54939
Adrian Czermak <adrian.czermak@univie.ac.at>
2021-04-19 14:25:51 +0200

    changed js to es6

--- amd/src/uicontroller.js
+++ amd/src/uicontroller.js
@@ -1,37 +1,34 @@
-define(['jquery'],
-    function ($) {
-        return {
-            init: function (colors) {
-                if (!document.fullscreenEnabled) {
-                    var fs_btn = document.getElementById('mod-wordcloud-fs-btn');
-                    fs_btn.style.display = "none";
-                }
-                $.mod_wordcloud_fs_toggle = function () {
-                    if (document.fullscreenElement === null) {
-                        var fs_element = document.getElementById('mod-wordcloud-content');
-                        fs_element.requestFullscreen();
-                    } else {
-                        document.exitFullscreen();
-                    }
-                };
-                document.onfullscreenchange = function () {
-                    var fs_icon = document.getElementById('mod-wordcloud-fs-icon');
-                    if (document.fullscreenElement === null) {
-                        fs_icon.className = fs_icon.className.replace(/\bfa-compress\b/g, "fa-expand");
-                    } else {
-                        fs_icon.className = fs_icon.className.replace(/\bfa-expand\b/g, "fa-compress");
-                    }
-                };
+export const init = colors => {
+    var stylerules = '';
+    var editCSS = document.createElement('style');
 
-                var stylerules = '';
-                var editCSS = document.createElement('style');
+    for (var i = 1; i <= colors.length; i++) {
+        stylerules += '.mod-wordcloud-w' + i + ' {color: #' + colors[i-1] + ';} \n';
+    }
 
-                for (var i = 1; i <= colors.length; i++) {
-                    stylerules += '.mod-wordcloud-w' + i + ' {color: #' + colors[i-1] + ';} \n';
-                }
+    editCSS.innerHTML = stylerules;
+    document.body.appendChild(editCSS);
 
-                editCSS.innerHTML = stylerules;
-                document.body.appendChild(editCSS);
-            }
-        };
-    });
+    if (!document.fullscreenEnabled) {
+        var fs_btn = document.getElementById('mod-wordcloud-fs-btn');
+        fs_btn.style.display = "none";
+    }
+
+    document.onfullscreenchange = function () {
+        var fs_icon = document.getElementById('mod-wordcloud-fs-icon');
+        if (document.fullscreenElement === null) {
+            fs_icon.className = fs_icon.className.replace(/\bfa-compress\b/g, "fa-expand");
+        } else {
+            fs_icon.className = fs_icon.className.replace(/\bfa-expand\b/g, "fa-compress");
+        }
+    };
+};
+
+export const mod_wordcloud_fs_toggle = () => {
+    if (document.fullscreenElement === null) {
+        var fs_element = document.getElementById('mod-wordcloud-content');
+        fs_element.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+};
