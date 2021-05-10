3b5a54eb71e1abfd2ad4651fef3f9d8a50ca12c2
Adrian Czermak <adrian.czermak@univie.ac.at>
2021-05-10 12:22:28 +0200

    Update version.php

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021042202;
+$plugin->version = 2021051000;
 $plugin->requires = 2019111800;
 $plugin->release = 'v1.0.0';
 $plugin->component = 'mod_wordcloud';
