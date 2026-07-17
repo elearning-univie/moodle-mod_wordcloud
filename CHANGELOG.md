a222ae195b323c3c09ee46389a00b7fd7f2011bd
Adrian Czermak <adrian.czermak@univie.ac.at>
2026-07-17 13:01:26 +0000

    Update 2 files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
-## 4.5.0
+
+## v4.5-r2
+* Bug fix to display Wordcloud activity in block timeline
+
+## 4.5.0/ v4.5-r1
 * Bug fixes and improvements
 * Moodle 4.5 compatible version
 * Thanks to the contributors: dpalou, james-cnz
--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024100700;
+$plugin->version = 2024100701;
 $plugin->requires = 2024042200;
-$plugin->release = 'v4.5.0';
+$plugin->release = 'v4.5-r2';
 $plugin->component = 'mod_wordcloud';
 $plugin->maturity = MATURITY_STABLE;
